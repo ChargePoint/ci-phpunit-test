90b673a982495316ad0a7b2175bd20841561f0d6
Achal-Aggarwal <theachalaggarwal@gmail.com>
2017-10-19 00:45:05 +0530

    Use GetOpt to take arguments

--- install.php
+++ install.php
@@ -8,25 +8,27 @@
  * @link       https://github.com/kenjis/ci-phpunit-test
  */
 
-require __DIR__ . '/Installer.php';
-
-$silent = false;
-$app_dir = 'application';
-
-// php install.php -s
-if (isset($argv[1]) && $argv[1] === '-s') {
-    $silent = true;
-
-    // php install.php -s app
-    if (isset($argv[2]) && is_dir($argv[2])) {
-        $app_dir = $argv[2];
+foreach ([__DIR__ . '/../../autoload.php', __DIR__ . '/vendor/autoload.php'] as $file) {
+    if (file_exists($file)) {
+        require $file;
+        break;
     }
 }
 
-// php install.php app
-if (isset($argv[1]) && is_dir($argv[1])) {
-    $app_dir = $argv[1];
-}
+require __DIR__ . '/Installer.php';
 
-$installer = new Installer($silent);
-$installer->install($app_dir);
+$getopt = new \GetOpt\GetOpt([
+
+    \GetOpt\Option::create('s', 'silent', \GetOpt\GetOpt::NO_ARGUMENT)
+        ->setDescription('Install silently'),
+
+    \GetOpt\Option::create('a', 'application', \GetOpt\GetOpt::OPTIONAL_ARGUMENT)
+        ->setDescription('Path to application directory')
+        ->setArgument(new \GetOpt\Argument('application', null, 'application')),
+
+]);
+
+$getopt->process();
+
+$installer = new Installer($getopt->getOption('silent'));
+$installer->install($getopt->getOption('application'));
